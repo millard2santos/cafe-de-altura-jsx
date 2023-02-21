7fa3532974565f7bf1b2831b5a0d42618e6888a6
millard2santos <millard2santos@gmail.com>
2023-02-21 17:52:13 +0100

    fixing

---
+++ src/components/ConditionCard/ConditionCard.jsx
@@ -0,0 +1,10 @@
+export const ConditionCard = ({img,text0,text1}) => {
+    return (
+        <article
+            className="card w-[320px] h-[232px] flex flex-col justify-center items-center gap-4 p-6 shadow bg-white rounded-lg">
+            <img src={img} alt="" />
+            <p className="text-lg font-semibold text-center">{text0}</p>
+            <p className="text-sm text-center">{text1}</p>
+        </article>
+    )
+}
